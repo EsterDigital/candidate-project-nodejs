7a3cdff2b05ead5a125bf7d1429379d3cb26f329
Mazzzy <shaikhmazahar@gmail.com>
2022-04-09 15:51:36 +0530

    Added security related packages to secure htpp header, sanitize xss, prevent param pollution and limiter for api requests

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require("cors")
 
-var userRoutes = require("./routes/user");
-var postRoutes = require("./routes/post");
-var commentRoutes = require("./routes/comment");
+// added extra packages
+var cors = require('cors')
+var rateLimit = require('express-rate-limit');
+var helmet = require('helmet');
+var xss = require('xss-clean');
+var hpp = require('hpp');
+
+// routes
+var userRoutes = require('./routes/user');
+var postRoutes = require('./routes/post');
+var commentRoutes = require('./routes/comment');
 
 var app = express();
-// enable cors
+
+// Enable cors
 app.use(cors());
+// Secure HTTP headers using helmet
+app.use(helmet());
+// Data sanitization against XSS
+app.use(xss());
+// Prevent parameter pollution
+app.use(hpp());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,10 +41,18 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Candidate Code Challenge - NodeJS API' });
 });
 
+// limiter for repeated requests to api routes
+var limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // allows 100 requests in 60 mins
+  message: 'Too many requests, try again later!',
+});
+app.use('/api', limiter);
+
 // api routes
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/posts", postRoutes);
-app.use("/api/v1/comments", commentRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/comments', commentRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
